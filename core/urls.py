2fe1b2abe11daa20a04185aff8d50dab563a477c
Jordan <jor.pobletem@duocuc.cl>
2021-06-04 15:48:14 -0400

    vistas y urls

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,18 @@
 from django.urls import path
-from .views import index, ilustraciones
+from .views import index, descripcion, diseños, formulario, graffitis, geolocalizacion, identidades, ilustraciones, login, portadas, recuperarcontra, register, api1
 
 urlpatterns = [
     path('index/', index, name="index"),
+    path('descripcion/', descripcion, name="descripcion"),
+    path('diseños/', diseños, name="diseños"),
+    path('formulario/', formulario, name="formulario"),
+    path('graffitis/', graffitis, name="graffitis"),
+    path('geolocalizacion/', geolocalizacion, name="geolocalizacion"),
+    path('identidades/', identidades, name="identidades"),
     path('ilustraciones/', ilustraciones, name="ilustraciones"),
+    path('login/', login, name="login"),
+    path('portadas/', portadas, name="portadas"),
+    path('recuperarcontra/', recuperarcontra, name="recuperarcontra"),
+    path('register/', register, name="register"),
+    path('api1/', api1, name="api1"),
 ]
--- core/views.py
+++ core/views.py
@@ -4,5 +4,38 @@ from django.shortcuts import render
 def index(request):
    return render(request, 'core/index.html')
 
+def descripcion(request):
+   return render(request, 'core/descripcion_ilustracion1.html')
+
+def diseños(request):
+   return render(request, 'core/diseños.html')
+
+def formulario(request):
+   return render(request, 'core/formulario.html')
+
+def graffitis(request):
+   return render(request, 'core/graffitis.html')
+
+def geolocalizacion(request):
+   return render(request, 'core/Geolocalizacion.html')
+
+def identidades(request):
+   return render(request, 'core/Identidades.html')
+
 def ilustraciones(request):
    return render(request, 'core/ilustraciones.html')
+
+def login(request):
+   return render(request, 'core/login.html')
+
+def portadas(request):
+   return render(request, 'core/portadas.html')
+
+def recuperarcontra(request):
+   return render(request, 'core/recuperarcontra.html')
+
+def register(request):
+   return render(request, 'core/registrar.html')
+
+def api1(request):
+   return render(request, 'core/uso_api.html')
