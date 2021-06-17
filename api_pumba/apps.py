9f496cbeab48970f8a73d05d54115245b79c2a7a
Jordan <jor.pobletem@duocuc.cl>
2021-06-17 13:20:45 -0400

    hola

---
+++ api_pumba/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ApiPumbaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'api_pumba'
---
+++ api_pumba/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from core.models import IlustracionIlu
+
+class IlustracionIluSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = IlustracionIlu
+        fields = ['idIlustracion', 'nombre', 'descripcionIlu', 'fecha','categoria','imagen']
